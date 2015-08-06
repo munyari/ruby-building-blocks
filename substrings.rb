9bc46c8e2f32cf554854f8544de27fc92b9a5bd8
Panashe Fundira <fundirap@gmail.com>
2015-08-06 17:18:18 -0400

    Give Hash default value of 0
    Much more concise, and does not require a new hash to be generated on each iteration

--- substrings.rb
+++ substrings.rb
@@ -1,19 +1,12 @@
 def substrings(str, dict)
-    hash = {}
+    hash = Hash.new(0)
     dict.each do |word|
-       str.downcase.scan(word.downcase) { hash = hash_add(hash, word) } 
+       str.downcase.scan(word.downcase) { hash[word] += 1 } 
     end
     hash
 end
 
-def hash_add(hash, key)
-    unless hash.include?(key)
-        hash[key] = 1
-    else
-        hash[key] = hash[key]+1
-    end
-    hash
-end
+
 
  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low",
  "own","part","partner","sit"]
